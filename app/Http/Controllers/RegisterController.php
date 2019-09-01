97d94d347cf9bda402573bbb9b60143324bf03e1
shentao <shentao@zz-med.com>
2019-09-01 15:17:26 +0800

    患者绑定接口

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Exceptions\DoctorAppException;
+use App\Http\Controllers\Controller;
+use App\Repositories\UsersRepositoryEloquent;
+use Illuminate\View\View;
+
+/**
+ * Class UsersController.
+ *
+ * @package namespace App\Http\Controllers\v1;
+ */
+class RegisterController extends Controller
+{
+
+    /**
+     * UsersController constructor.
+     *
+     * @param UsersRepositoryEloquent $repository
+     */
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        return View('register');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// 用户注册
+Route::get('user/register/{docId}', 'RegisterController@index');
