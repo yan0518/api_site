4de120a7784cb56615ac13060703a172a2103414
shentao <shentao@zz-med.com>
2019-09-01 16:20:43 +0800

    发送成功页面

--- resources/views/registerSucceed.blade.php
+++ resources/views/registerSucceed.blade.php
@@ -6,16 +6,15 @@
     <title>报名成功引导页</title>
     <script src="{{env('APP_URL')}}/js/rem.js"></script>
     <link rel="stylesheet" href="{{env('APP_URL')}}/css/ResetCss.css">
-    <link rel="stylesheet" href="{{env('APP_URL')}}/css/index.css">
+    <link rel="stylesheet" href="{{env('APP_URL')}}/css/index.css?v2">
 </head>
 <body>
 <div class="content1">
     <img src="{{env('APP_URL')}}/img/未注册绑定微信copy-banner图@3x.png" class="banner">
-    <div>
-        <img src="{{env('APP_URL')}}/img/注册成功引导页-背景图标@3x.png" class="successBackground">
-        <img src="{{env('APP_URL')}}/img/注册成功引导页-加入我们@3x.png" class="joinUs">
-        <div class="word">恭喜您成功加入MMC</div>
-    </div>
+
+    <img src="{{env('APP_URL')}}/img/注册成功引导页-背景图标@3x.png" class="successBackground">
+    <img src="{{env('APP_URL')}}/img/注册成功引导页-加入我们@3x.png" class="joinUs">
+    <div class="word">恭喜您报名成功</div>
 </div>
 </body>
 </html>
