fc903d35732e1876166d68c6cf1aed28e1778393
shentao <shentao@zz-med.com>
2019-09-02 22:36:05 +0800

    微信绑定

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -13,6 +13,74 @@ use Log;
 class WeChatController extends Controller
 {
 
+
+    public function __construct(WechatUserRepositoryEloquent $wechatUserRepository)
+    {
+        $this->wechatUserRepository = $wechatUserRepository;
+    }
+
+
+    /**
+     * event 操作日志
+     * @param $message
+     * @return array|null|string
+     */
+    public function EventProcess($message)
+    {
+        $openid = $message->FromUserName;
+        switch ($message->Event) {
+            //订阅
+            case 'subscribe':
+
+                $wechat = app('wechat.official_account');
+                $userService = $wechat->user;
+
+                $wechatUserFromWX = $userService->get($openid);
+
+                $data['subscribe'] = $wechatUserFromWX['subscribe'];
+                $data['openID'] = $openid;
+                $data['nickname'] = $wechatUserFromWX['nickname'];
+                $data['sex'] = $wechatUserFromWX['sex'];
+                $data['city'] = $wechatUserFromWX['city'];
+                $data['country'] = $wechatUserFromWX['country'];
+                $data['province'] = $wechatUserFromWX['province'];
+                $data['language'] = $wechatUserFromWX['language'];
+                $data['headimgurl'] = $wechatUserFromWX['headimgurl'];
+                $data['subscribe_time'] = $wechatUserFromWX['subscribe_time'];
+                $data['remark'] = $wechatUserFromWX['remark'];
+                $data['groupid'] = $wechatUserFromWX['groupid'];
+
+                $this->wechatUserRepository->updateOrCreate(['openID' => $openid], $data);
+
+
+//                $material = new Material('mpnews', '1DFH_WiaEfYx78B03Or01B_ekoF_sXi3x7xNrF9Cz5g');
+//                Log::info($material);
+//                return $material;
+                break;
+            //取消订阅
+            case 'unsubscribe':
+                $this->UnsubscribeProcess($openid);
+                return '欢迎取消';
+                break;
+            //已订阅用户扫码
+            case 'SCAN':
+                break;
+            //上报地理位置事件
+            case 'LOCATION':
+                break;
+            //点击菜单拉取消息时的事件推送
+            case 'CLICK':
+                break;
+            //点击菜单跳转链接时的事件推送
+            case 'VIEW':
+                break;
+            default:
+                # code...
+                break;
+        }
+        return NULL;
+    }
+
     /**
      * 微信服务器连接
      * @param Request $request
@@ -26,6 +94,7 @@ class WeChatController extends Controller
             log::info($message);
             switch ($message->MsgType) {
                 case 'event':
+                    return self::EventProcess($message);
                     break;
                 case 'text':
                     $items = [
