ea8b1490f8920553b03decf9b74992c12e9c62c7
shentao <shentao@zz-med.com>
2019-09-03 00:23:05 +0800

    微信绑定

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -112,7 +112,7 @@ class WeChatController extends Controller
                 case 'text':
                     $items[] = [
                         new NewsItem([
-                            'title' => '',
+                            'title' => '微信绑定',
                             'description' => '',
                             'url' => 'http://api.pigzu.com/user/register/bd6325a5-27b8-47d6-8dc1-25be757ae94f',
                             'image' => 'https://zz-med-national.oss-cn-hangzhou.aliyuncs.com/wechat/banner.png',
