66778e9c5aaeb28fd9a73705426fc0a59c18ee28
shentao <shentao@zz-med.com>
2019-09-08 20:48:37 +0800

    微信绑定

--- resources/views/registerSucceed.blade.php
+++ resources/views/registerSucceed.blade.php
@@ -11,7 +11,6 @@
 <body>
 <div class="content1">
     <img src="https://zz-med-national.oss-cn-hangzhou.aliyuncs.com/CminiProgram/images/TYCL.jpg" class="banner">
-
     <img src="{{env('APP_URL')}}/img/注册成功引导页-背景图标@3x.png" class="successBackground">
     <img src="{{env('APP_URL')}}/img/注册成功引导页-加入我们@3x.png" class="joinUs">
     <div class="word">恭喜您报名成功</div>
